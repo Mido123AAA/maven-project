08dc4c9329bb8deaff4ba3cc38ccac15e3b81e43
Mido123AAA <97283800+Mido123AAA@users.noreply.github.com>
2022-01-17 17:12:25 +0100

    Update Greeter.java
    add changes

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,8 @@ public class Greeter {
 
   }
   /**
-  * @param someone
-  * @return greetings
+  * @param someone the name of a person
+  * @return greeting string
   */
 
   public final String greet(final String someone) {
